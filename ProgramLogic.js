6624c78bffda20abd9b34309e460f7b6f936624d
Agustin Ordoñez <aordonez@frba.utn.edu.ar>
2022-10-03 10:03:00 -0300

    Cambios al evento de cálculo del subtotal

--- ProgramLogic.js
+++ ProgramLogic.js
@@ -5,5 +5,9 @@ function calcularTotal(){
 function calcularSubTotal(numeroDeGrupo) {
     let cantidadDeProductos=parseInt(document.getElementsByName("cantidadProducto")[numeroDeGrupo].value)
     let precioProducto =parseInt(document.getElementsByName("precioUnitario")[numeroDeGrupo].value)
+
+    if(!isNaN(precioProducto) && !isNaN(cantidadDeProductos)){
     document.getElementsByName("subtotal").item(numeroDeGrupo).innerText=precioProducto*cantidadDeProductos
+    }
+
 }
